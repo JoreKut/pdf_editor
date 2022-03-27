2b872b99bcd9d4d6bf3ea149324b39d390675d25
JoreKut <37114216+JoreKut@users.noreply.github.com>
2022-03-27 18:39:26 +0300

    changed file_font_size

--- util/SETTING.py
+++ util/SETTING.py
@@ -6,5 +6,5 @@ hsv_min = np.array((0, 0, 0), np.uint8)
 hsv_max = np.array((155, 55, 55), np.uint8)
 
 
-file_font_size = 27
+file_font_size = 25
 file_font = ImageFont.truetype("arial.ttf", file_font_size)
