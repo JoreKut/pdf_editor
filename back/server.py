297ca92e3f44c4acde7ee05f866edd33d7e2b8a9
JoreKut <37114216+JoreKut@users.noreply.github.com>
2022-03-27 18:46:45 +0300

    added preliminary unwrapping of internal data

--- back/server.py
+++ back/server.py
@@ -1,14 +1,16 @@
+from dbm import dumb
 from flask import Flask 
-from flask import jsonify, request
-
+from flask import request
+from util.fill_in_pdf import make_pdf
 
 app = Flask(__name__)
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/create-pdf', methods=['POST'])
 def login():
-    data = request.form.to_dict(flat=False)
-    return jsonify(data)
-
+    data = dict(request.form.to_dict(flat=False))
+    data = dict([(k, v[0].upper()) for (k, v) in data.items()])
+    make_pdf(data)
+    return data
 
 if __name__ == '__main__':
     app.run(debug=True)
