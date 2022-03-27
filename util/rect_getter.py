7af9e0517fe2a39b9af02e4934d0fc86a03abf87
JoreKut <37114216+JoreKut@users.noreply.github.com>
2022-03-27 18:43:48 +0300

    changed the order of rectangles

--- util/rect_getter.py
+++ util/rect_getter.py
@@ -1,33 +1,23 @@
-from asyncio.windows_events import NULL
 import cv2 as cv
-from SETTING import *
+from util.SETTING import *
 
-def get_rects(page_paht):
+def get_rects(page_path):
     
-    img = cv.imread(page_paht)
+    img = cv.imread(page_path)
     hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)  # меняем цветовую модель с BGR на HSV
     thresh = cv.inRange(hsv, hsv_min, hsv_max)  # применяем цветовой фильтр
-    contours0, f = cv.findContours(thresh.copy(), cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    contours0, hierarchy = cv.findContours(thresh.copy(), cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
     resulting_rects = []
 
-    previouse_rect = NULL
     # перебираем все найденные контуры в цикле
     for cnt in contours0:
         rect = cv.minAreaRect(cnt)  # пытаемся вписать прямоугольник
-
-        if  min(rect[1][0], rect[1][1]) < 20:
+        if rect[1][0] < 37 or rect[1][0] > 50:
             continue
-        if previouse_rect is not NULL:
-            # сравнивае предыдущий объект
-            # изначально из одного квадрата мы получаем два объекта:
-            #   внешний контур
-            #   внутренний контур
-            # толщина равна 0.5 по определению
-            if abs(previouse_rect[0][0] - rect[0][0]) < 2 and abs(previouse_rect[0][1] - rect[0][1]) < 2:
-                continue
 
         resulting_rects.append(rect)
-        previouse_rect = rect
-
+    
+    resulting_rects.reverse()
+    
     return resulting_rects
