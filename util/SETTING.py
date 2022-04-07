f67e47895f7f01178e8fb7e4f0d76dc70127f7b5
JoreKut <37114216+JoreKut@users.noreply.github.com>
2022-04-07 13:29:28 +0300

    hsv_max edited

--- util/SETTING.py
+++ util/SETTING.py
@@ -3,7 +3,7 @@ from PIL import ImageFont
 import numpy as np
 
 hsv_min = np.array((0, 0, 0), np.uint8)
-hsv_max = np.array((179, 255, 220), np.uint8)
+hsv_max = np.array((179, 255, 210), np.uint8)
 
 file_font_size = 25
 font_fill = (30, 30, 30)
